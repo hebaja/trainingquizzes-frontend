112ebae8cdf8b61094e763582859e88ee58bd0fc
hebaja <hebaja@yahoo.com.br>
2022-11-26 22:50:07 -0300

    commit for local

--- src/http.js
+++ src/http.js
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'production') url = 'https://trainingquizzes.com:844
 else if(isMobile()) url = 'http://192.168.1.100:8080'
 else url = 'http://localhost:8080'
 
-// url = 'http://192.168.122.2:8080'
+url = 'http://192.168.122.2:8080'
 // url = 'https://trainingquizzes.com:8443'
 
 console.log(url)
